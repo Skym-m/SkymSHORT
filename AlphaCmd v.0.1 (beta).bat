7db794f01ba908b63ea585bec7131c3cd43f6eef
Skym <70912241+Skym-m@users.noreply.github.com>
2021-11-24 22:00:33 +0100

    Update AlphaCmd v.0.1 (beta).bat

--- AlphaCmd v.0.1 (beta).bat
+++ AlphaCmd v.0.1 (beta).bat
@@ -19,7 +19,6 @@ if "%choix%"=="lance Google" goto :b
 if "%choix%"=="Lance google" goto :b
 if "%choix%"=="lance google" goto :b
 if "%choix%"=="Lance Google" goto :b
-if "%choix%"=="Lance GooGle" goto :b
 
 if "%choix%"=="Lance youtube" goto :c
 if "%choix%"=="Lance Youtube" goto :c
